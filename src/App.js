2be494d4f9aea0485568163624f61e38724c931d
Пахомов Евгений <nightmare23@yandex.ru>
2024-09-26 22:12:37 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import HomePage from "./components/HomePage";
 function App() {
   return (
     <div className="App">
-
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
